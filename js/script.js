9ce47fd991b02dd5460cdeaf966235fa7b6c7616
Gabriel Marques <gabrielmarques.if@gmail.com>
2024-01-31 16:16:35 -0300

    Funções dos botões finalizadas
    Ajuste na imagem (set)

--- js/script.js
+++ js/script.js
@@ -1,11 +1,37 @@
-const play = document.querySelector('button.play')
-const pause = document.querySelector('button.pause')
+const buttonPlay = document.querySelector('button.play');
+const buttonPause = document.querySelector('button.pause');
+const buttonStop = document.querySelector('button.stop');
+const buttonSet = document.querySelector('button.set')
 
-function clickPlay() {
-    play.classList.add('hide')
-    pause.classList.remove('hide')
+const buttonSoundOn = document.querySelector('button.soundOn')
+const buttonSoundOff = document.querySelector('button.soundOff')
+
+
+function clickButtonPlay() {
+    buttonPlay.classList.add('hide')
+    buttonPause.classList.remove('hide')
+
+    buttonStop.classList.remove('hide')
+    buttonSet.classList.add('hide')
 }
-function clickPause() {
-    pause.classList.add('hide')
-    play.classList.remove('hide')
+function clickButtonPause() {
+    buttonPause.classList.add('hide')
+    buttonPlay.classList.remove('hide')
+}
+function clickButtonStop(){
+    buttonPlay.classList.remove('hide')
+    buttonPause.classList.add('hide')
+
+    buttonSet.classList.remove('hide')
+    buttonStop.classList.add('hide')
+}
+
+
+function clickButtonSoundOn(){
+    buttonSoundOn.classList.add('hide')
+    buttonSoundOff.classList.remove('hide')
+}
+function clickButtonSoundOff(){
+    buttonSoundOn.classList.remove('hide')
+    buttonSoundOff.classList.add('hide')
 }
