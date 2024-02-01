a4de022c121d0b588e49ffe52289231ae6c7b57b
Gabriel Marques <gabrielmarques.if@gmail.com>
2024-02-01 17:16:26 -0300

    Lógica adicionada
    Para contar os segundos do relógio

--- js/script.js
+++ js/script.js
@@ -1,11 +1,28 @@
 const buttonPlay = document.querySelector('button.play');
 const buttonPause = document.querySelector('button.pause');
 const buttonStop = document.querySelector('button.stop');
-const buttonSet = document.querySelector('button.set')
+const buttonSet = document.querySelector('button.set');
+const minutesDisplay = document.querySelector('span.minutes');
+const secondsDisplay = document.querySelector('span.seconds');
 
-const buttonSoundOn = document.querySelector('button.soundOn')
-const buttonSoundOff = document.querySelector('button.soundOff')
+let minutes
 
+const buttonSoundOn = document.querySelector('button.soundOn');
+const buttonSoundOff = document.querySelector('button.soundOff');
+
+function countDown(){
+    setTimeout(function() {
+        let seconds = Number(secondsDisplay.textContent)
+
+        if(seconds <= 0){
+            seconds = 60
+        }
+
+        secondsDisplay.textContent = seconds - 1
+        countDown()
+    }, 1000)
+
+}
 
 function clickButtonPlay() {
     buttonPlay.classList.add('hide')
@@ -13,6 +30,8 @@ function clickButtonPlay() {
 
     buttonStop.classList.remove('hide')
     buttonSet.classList.add('hide')
+
+    countDown()
 }
 function clickButtonPause() {
     buttonPause.classList.add('hide')
@@ -25,7 +44,13 @@ function clickButtonStop(){
     buttonSet.classList.remove('hide')
     buttonStop.classList.add('hide')
 }
+function clickButtonSet(){
+    minutes = Number(window.prompt(`Quantos minutos?`))
+    minutesDisplay.textContent = minutes
 
+
+    
+}
 
 function clickButtonSoundOn(){
     buttonSoundOn.classList.add('hide')
