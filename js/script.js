ece01c1e323f50fe727bfb440bff09eac5fc15d0
Gabriel Marques <gabrielmarques.if@gmail.com>
2024-02-03 18:47:56 -0300

    Lógica  aplicada nos contadores

--- js/script.js
+++ js/script.js
@@ -10,18 +10,31 @@ let minutes
 const buttonSoundOn = document.querySelector('button.soundOn');
 const buttonSoundOff = document.querySelector('button.soundOff');
 
-function countDown(){
-    setTimeout(function() {
+function countDown() {
+    setTimeout(function () {
         let seconds = Number(secondsDisplay.textContent)
+        let minutes = Number(minutesDisplay.textContent)
 
-        if(seconds <= 0){
-            seconds = 60
+        if (seconds <= 0) {
+            seconds = 2
+
+            minutesDisplay.textContent = String(minutes - 1).padStart(2, "0")
         }
 
-        secondsDisplay.textContent = seconds - 1
+        secondsDisplay.textContent = String(seconds - 1).padStart(2, "0")
+
+        if (minutes <= 0) {
+            buttonPlay.classList.add('hide')
+            buttonPause.classList.remove('hide')
+
+            buttonStop.classList.remove('hide')
+            buttonSet.classList.add('hide')
+
+            return
+        }
+
         countDown()
     }, 1000)
-
 }
 
 function clickButtonPlay() {
@@ -37,26 +50,23 @@ function clickButtonPause() {
     buttonPause.classList.add('hide')
     buttonPlay.classList.remove('hide')
 }
-function clickButtonStop(){
+function clickButtonStop() {
     buttonPlay.classList.remove('hide')
     buttonPause.classList.add('hide')
 
     buttonSet.classList.remove('hide')
     buttonStop.classList.add('hide')
 }
-function clickButtonSet(){
+function clickButtonSet() {
     minutes = Number(window.prompt(`Quantos minutos?`))
-    minutesDisplay.textContent = minutes
-
-
-    
+    minutesDisplay.textContent = String(minutes).padStart(2, "0")
 }
 
-function clickButtonSoundOn(){
+function clickButtonSoundOn() {
     buttonSoundOn.classList.add('hide')
     buttonSoundOff.classList.remove('hide')
 }
-function clickButtonSoundOff(){
+function clickButtonSoundOff() {
     buttonSoundOn.classList.remove('hide')
     buttonSoundOff.classList.add('hide')
 }
